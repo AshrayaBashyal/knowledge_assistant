22a02f7725405eec0d0d4b45e9bb9865db9cc9e6
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-07-19 14:39:40 +0545

    add url to (re)index notes

--- retrieval/urls.py
+++ retrieval/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from retrieval.views import DocumentIndexView
+from retrieval.views import DocumentIndexView, NoteIndexView
 
 app_name = "retrieval"
 
 urlpatterns = [
     path("documents/<int:pk>/index/", DocumentIndexView.as_view(), name="document-index"),
+    path("notes/<int:pk>/index/", NoteIndexView.as_view(), name="note-index"),
 ]
