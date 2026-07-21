8669f1c650ac15b11aa7606973e999b5489d3b94
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-07-21 18:09:11 +0545

    setup postgres database--Supabase

--- config/settings.py
+++ config/settings.py
@@ -67,11 +67,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 ASGI_APPLICATION = "config.asgi.application"
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,  # Keeps connections alive for 10 minutes to boost speed
+    )
 }
 
 AUTH_USER_MODEL = "accounts.User"
