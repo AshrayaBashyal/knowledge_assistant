85e276e6b32ae9758864aa2aa47d38aeb86c50a5
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-07-23 16:13:48 +0545

    add views to list and delete flashcard set of a user

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
 
-# Create your views here.
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from documents.models import Document
+from notes.models import Note
+
+from flashcards.serializers import FlashcardSerializer, FlashcardSetSerializer, FlashcardSetDetailSerializer, GenerateFlashcardsInputSerializer
+from flashcards.models import Flashcard, FlashcardSet
+
+
+class FlashcardSetListView(generics.ListAPIView):
+
+    serializer_class = [FlashcardSetSerializer]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return FlashcardSet.objects.filter(user=self.request.user)
+    
+
+class FlashcardSetDetailView(generics.RetrieveDestroyAPIView):
+ 
+    serializer_class = FlashcardSetDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
+ 
+    def get_queryset(self):
+        return FlashcardSet.objects.filter(user=self.request.user)
+    
+
